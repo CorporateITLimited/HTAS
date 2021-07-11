5078b7b6fc0dbed7c52b79bfe07a8fe5dd17d503
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2021-07-11 12:03:01 +0600

    new manager added
    plot and ownership

---
+++ DCBHoldingTax/HoldingTaxWebApp/Manager/DBO/OwnershipSourceManager.cs
@@ -0,0 +1,24 @@
+﻿using HoldingTaxWebApp.Gateway.DBO;
+using HoldingTaxWebApp.Models.DBO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Manager.DBO
+{
+    public class OwnershipSourceManager
+    {
+        private readonly OwnershipSourceGateway _ownershipSourceGateway;
+
+        public OwnershipSourceManager()
+        {
+            _ownershipSourceGateway = new OwnershipSourceGateway();
+        }
+
+        public List<OwnershipSource> GetAllOwnershipSource()
+        {
+            return _ownershipSourceGateway.GetAllOwnershipSource();
+        }
+    }
+}
---
+++ DCBHoldingTax/HoldingTaxWebApp/Manager/Plots/PlotManager.cs
@@ -0,0 +1,23 @@
+﻿using HoldingTaxWebApp.Gateway.Plots;
+using HoldingTaxWebApp.Models.Plots;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Manager.Plots
+{
+    public class PlotManager
+    {
+        private readonly PlotGateway _plotGateway;
+        public PlotManager()
+        {
+            _plotGateway = new PlotGateway();
+        }
+
+        public List<Plot> GetAllPlot()
+        {
+            return _plotGateway.GetAllPlot();
+        }
+    }
+}
