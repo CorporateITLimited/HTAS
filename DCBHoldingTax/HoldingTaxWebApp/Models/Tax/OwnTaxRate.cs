cf1adce58a3595c500f2c3d834e72bca4e9651ac
Rubaid <survivorshanto@gmail.com>
2021-07-13 23:51:56 +0600

    HoldingTaxGateway

--- DCBHoldingTax/HoldingTaxWebApp/Models/Tax/OwnTaxRate.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Models/Tax/OwnTaxRate.cs
@@ -9,17 +9,29 @@ namespace HoldingTaxWebApp.Models.Tax
 {
     public class OwnTaxRate
     {
+        
+        [Display(Name = "নিজস্ব করের হারের আইডি")]
         public int OwnTaxRateId { get; set; }
         [Display(Name ="সামরিক/বেসামরিক")]
         public int? Mill_Civil { get; set; }
+    
+        [Display(Name = "অঞ্চল এসএফ")]
         public decimal? AreaSF { get; set; }
+        [Display(Name = "পরিমাণ")]
         public decimal? Amount { get; set; }
+        [Display(Name = "মন্তব্য")]
         public string Remarks { get; set; }
-        public DateTime CreateDate { get; set; }
+        [Display(Name = "তারিখ তৈরি করুন")]
+        public DateTime? CreateDate { get; set; }
+        [Display(Name = "তৈরিকারী")]
         public int? CreatedBy { get; set; }
-        public DateTime LastUpdated { get; set; }
+        [Display(Name = "সর্বশেষ সংষ্করণ")]
+        public DateTime? LastUpdated { get; set; }
+        [Display(Name = "সর্বশেষ সংষ্করণ করেছেন")]
         public int? LastUpdatedBy { get; set; }
+        [Display(Name = "সক্রিয়")]
         public bool? IsActive { get; set; }
+        [Display(Name = "মুছে ফেলা ")]
         public bool? IsDeleted { get; set; }
     }
 }
