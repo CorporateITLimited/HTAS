d64c31c487c9075bff90b7ce80df2a8b5a1a43cc
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2021-07-14 14:29:59 +0600

    commited
    holding delete and insert

--- DCBHoldingTax/HoldingTaxWebApp/Gateway/Holding/HoldingGateway.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Gateway/Holding/HoldingGateway.cs
@@ -675,6 +675,60 @@ namespace HoldingTaxWebApp.Gateway.Holding
             }
         }
 
+        public int DeleteHoldersFlatDataByHolderId(int HolderId)
+        {
+            try
+            {
+                Sql_Query = "[Holding].[spHolderFlatMaster]";
+                Sql_Command = new SqlCommand
+                {
+                    CommandText = Sql_Query,
+                    Connection = Sql_Connection,
+                    CommandType = CommandType.StoredProcedure
+                };
+                Sql_Command.Parameters.Clear();
+
+                Sql_Command.Parameters.Add("@StatementType", SqlDbType.NVarChar).Value = CommonConstantHelper.Delete;
+                Sql_Command.Parameters.Add("@HolderId", SqlDbType.Int).Value = HolderId;
+
+                SqlParameter result = new SqlParameter
+                {
+                    ParameterName = "@result",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Output
+                };
+                Sql_Command.Parameters.Add(result);
+
+                Sql_Connection.Open();
+                int rowAffected = Sql_Command.ExecuteNonQuery();
+                Sql_Connection.Close();
+
+                int resultOutPut = int.Parse(result.Value.ToString());
+
+                return resultOutPut;
+            }
+            catch (SqlException exception)
+            {
+                for (int i = 0; i < exception.Errors.Count; i++)
+                {
+
+                    ErrorMessages.Append("Index #" + i + "\n" +
+                        "Message: " + exception.Errors[i].Message + "\n" +
+                        "Error Number: " + exception.Errors[i].Number + "\n" +
+                        "LineNumber: " + exception.Errors[i].LineNumber + "\n" +
+                        "Source: " + exception.Errors[i].Source + "\n" +
+                        "Procedure: " + exception.Errors[i].Procedure + "\n");
+                }
+                throw new Exception(ErrorMessages.ToString());
+            }
+
+            finally
+            {
+                if (Sql_Connection.State == ConnectionState.Open)
+                    Sql_Connection.Close();
+            }
+        }
+
         #endregion
 
         #region frontend queries
