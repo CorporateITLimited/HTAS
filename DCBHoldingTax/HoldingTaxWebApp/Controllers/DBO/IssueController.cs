c9212299899527212596f8cc3c0452c153725f63
Hasan <Hasan@citl.com.bd>
2021-07-18 14:31:12 +0600

    commit issue index

--- DCBHoldingTax/HoldingTaxWebApp/Controllers/DBO/IssueController.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Controllers/DBO/IssueController.cs
@@ -1,6 +1,7 @@
 ﻿using HoldingTaxWebApp.Manager.DBO;
 using HoldingTaxWebApp.Manager.Holding;
 using HoldingTaxWebApp.Models.DBO;
+using HoldingTaxWebApp.ViewModels.DBO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -73,7 +74,67 @@ namespace HoldingTaxWebApp.Controllers.DBO
         // GET: Issue/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            try
+            {
+                var IssueList = _IssueManager.GetIssueById(id);
+                var IssueDetailsList = _IssueManager.GetAllIssueDetailsByIssueId(id);
+
+                if (IssueList == null)
+                    return HttpNotFound();
+              
+
+
+                List<IssueDetails> IssueDetailsVM = new List<IssueDetails>();
+                foreach (var item in IssueDetailsList)
+                {
+                    IssueDetails IssueDetails = new IssueDetails()
+                    {
+                        Doc1 = item.Doc1,
+                        Doc2 = item.Doc2,
+                        IsRead = item.IsRead,
+                        IssueDetailsId = item.IssueDetailsId,
+                        IssueId = item.IssueId,
+                        MsgDate = item.MsgDate,
+                        MsgDetails = item.MsgDetails,
+                        StringMsgDate = $"{item.MsgDate:dd/MM/yyyy}",
+                        
+                    };
+                    IssueDetailsVM.Add(IssueDetails);
+                }
+
+                IssueCombineVM IssueVM = new IssueCombineVM()
+                {
+                    CreateDate = IssueList.CreateDate,
+                    CreatedBy = IssueList.CreatedBy,
+                    IssueId = IssueList.IssueId,
+                    CreatedByUserName = IssueList.CreatedByUserName,
+                    HolderId = IssueList.HolderId,
+                    HolderName = IssueList.HolderName,
+                    IsActive = IssueList.IsActive,
+                    IsDeleted = IssueList.IsDeleted,
+                    IssueDetails = IssueDetailsVM,
+                    LastUpdated = IssueList.LastUpdated,
+                    LastUpdatedBy = IssueList.LastUpdatedBy,
+                    Remarks = IssueList.Remarks,
+                    SolvedDate = IssueList.SolvedDate,
+                    StatusName = IssueList.StatusName,
+                    StatusTypeId = IssueList.StatusTypeId,
+                    StringSolvedDate = $"{IssueList.SolvedDate:dd/MM/yyyy}",
+                    Subject = IssueList.Subject,
+                    UpdatedByUserName = IssueList.UpdatedByUserName
+
+                };
+
+
+
+                return View(IssueVM);
+            }
+            catch (Exception exception)
+            {
+                TempData["EM"] = "error | " + exception.Message.ToString();
+                return View();
+                //return RedirectToAction("Error", "Home");
+            }
         }
 
         // GET: Issue/Create
