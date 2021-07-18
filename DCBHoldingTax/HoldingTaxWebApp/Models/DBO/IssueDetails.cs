bb07e307a0ce20bf863055d90973d3fe45371e46
Hasan <Hasan@citl.com.bd>
2021-07-18 13:32:06 +0600

    commit issue

---
+++ DCBHoldingTax/HoldingTaxWebApp/Models/DBO/IssueDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Models.DBO
+{
+    public class IssueDetails
+    {
+        public int IssueDetailsId { get; set; }
+        public int IssueId { get; set; }
+        public string MsgDetails { get; set; }
+        public DateTime? MsgDate { get; set; }
+        public string StringMsgDate { get; set; }
+        public string Doc1 { get; set; }
+        public string Doc2 { get; set; }
+
+        public bool? IsRead { get; set; }
+    }
+}
---
+++ DCBHoldingTax/HoldingTaxWebApp/ViewModels/DBO/IssueCombineVM.cs
@@ -0,0 +1,44 @@
+﻿using HoldingTaxWebApp.Models.DBO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.ViewModels.DBO
+{
+    public class IssueCombineVM
+    {
+        public int IssueId { get; set; }
+        public int HolderId { get; set; }
+        public int StatusTypeId { get; set; }
+        public string Subject { get; set; }
+        //public string Details { get; set; }
+        //public string Doc1 { get; set; }
+        //public string Doc2 { get; set; }
+        public DateTime? SolvedDate { get; set; }
+        public string StringSolvedDate { get; set; }
+        public string Remarks { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public int? LastUpdatedBy { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public string CreatedByUserName { get; set; }
+        public string UpdatedByUserName { get; set; }
+
+        ////out of table
+        public string HolderName { get; set; }
+        public string StatusName { get; set; }
+
+        public List<IssueDetails> IssueDetails { get; set; }
+
+        public IssueCombineVM()
+        {
+            IssueDetails = new List<IssueDetails>();
+        }
+
+
+    }
+}
