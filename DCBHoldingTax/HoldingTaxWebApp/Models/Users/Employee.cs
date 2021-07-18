8bc772403389b1fbc01254e72053031f479a5966
Rubaid <survivorshanto@gmail.com>
2021-07-19 04:41:39 +0600

    Update Employee.cs

--- DCBHoldingTax/HoldingTaxWebApp/Models/Users/Employee.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Models/Users/Employee.cs
@@ -8,41 +8,52 @@ namespace HoldingTaxWebApp.Models.Users
 {
     public class Employee
     {
+        [Display(Name = "কর্মকর্তার আইডি")]
         public int EmpolyeeId { get; set; }
-        [Display(Name = "Employee Name")]
+        [Display(Name = "কর্মকর্তার নাম")]
         public string EmployeeName { get; set; }
 
-        [Display(Name = "Designation")]
+        [Display(Name = "পদবি ")]
         public int DesignationId { get; set; }
-        [Display(Name = "Designation Name")]
+        [Display(Name = "পদবি নাম")]
         public string DesignationName { get; set; }
-        [Display(Name = "Father Name")]
+        [Display(Name = "বাবার নাম")]
         public string FatherName { get; set; }
-        [Display(Name = "Mother Name")]
+        [Display(Name = "মায়ের নাম")]
         public string MotherName { get; set; }
-        [Display(Name = "Contact No")]
+        [Display(Name = "যোগাযোগের নম্বর")]
         public string ContactNo { get; set; }
+
+        [Display(Name = "ইমেল")]
         public string Email { get; set; }
+        [Display(Name = "জন্ম তারিখ")]
         public DateTime? DOB { get; set; }
-        [Display(Name = "DOB")]
+        [Display(Name = "জন্ম তারিখ")]
         public string StringDOB { get; set; }
+        [Display(Name = "লিঙ্গ")]
         public string Sex { get; set; }
+        [Display(Name = "এনআইডি")]
         public string NID { get; set; }
-        [Display(Name = "Employee Address")]
+        [Display(Name = "কর্মচারী ঠিকানা")]
         public string EmployeeAddress { get; set; }
+        [Display(Name = "মন্তব্য")]
         public string Remarks { get; set; }
-        [Display(Name = "Create Date")]
+        [Display(Name = "তারিখ তৈরি করুন")]
         public DateTime? CreateDate { get; set; }
+        [Display(Name = "তৈরিকারী")]
         public int? CreatedBy { get; set; }
-        [Display(Name = "Created By ")]
+        [Display(Name = "তৈরিকারী")]
         public string CreatedByUserName { get; set; }
-        [Display(Name = "Updated Date ")]
+        [Display(Name = "আপডেটের তারিখ")]
         public DateTime? LastUpdated { get; set; }
+        [Display(Name = "সর্বশেষ আপডেট করেছেন")]
         public int? LastUpdatedBy { get; set; }
 
-        [Display(Name = "Updated By")]
+        [Display(Name = "আপডেট হয়েছে")]
         public string UpdatedByUserName { get; set; }
+        [Display(Name = "সক্রিয়")]
         public bool? IsActive { get; set; }
+        [Display(Name = "মুছে ফেলা ")]
         public bool? IsDeleted { get; set; }
 
     }
