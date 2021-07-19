c3aa40aee64de5d559ac49145dafd75b7b3c0079
Rubaid <survivorshanto@gmail.com>
2021-07-19 09:25:27 +0600

    Update OwnershipSource.cs

--- DCBHoldingTax/HoldingTaxWebApp/Models/DBO/OwnershipSource.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Models/DBO/OwnershipSource.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,22 @@ namespace HoldingTaxWebApp.Models.DBO
 {
     public class OwnershipSource
     {
+        [Display(Name = "মালিকানা সোর্স আইডি")]
         public int OwnershipSourceId { get; set; }
+
+        [Display(Name = "উৎসের নাম")]
         public string SourceName { get; set; }
+        [Display(Name = "তারিখ তৈরি করুন")]
         public DateTime? CreateDate { get; set; }
+        [Display(Name = "তৈরিকারী")]
         public int? CreatedBy { get; set; }
+        [Display(Name = "সর্বশেষ সংষ্করণ")]
         public DateTime? LastUpdated { get; set; }
+        [Display(Name = "সর্বশেষ সংষ্করণ করেছেন")]
         public int? LastUpdatedBy { get; set; }
+        [Display(Name = "সক্রিয়")]
         public bool? IsActive { get; set; }
+        [Display(Name = "মুছে ফেলা ")]
         public bool? IsDeleted { get; set; }
     }
 }
