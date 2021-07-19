241e7f56b68fa3e93e72dda1188ff02ef6161fa5
Rubaid <survivorshanto@gmail.com>
2021-07-19 09:36:44 +0600

    Update LeaseQuota.cs

--- DCBHoldingTax/HoldingTaxWebApp/Models/DBO/LeaseQuota.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Models/DBO/LeaseQuota.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,14 +8,23 @@ namespace HoldingTaxWebApp.Models.DBO
 {
     public class LeaseQuota
     {
+        [Display(Name = "ইজারা কোটা আইডি")]
         public int LeaseQuotaId { get; set; }
+        [Display(Name = "ইজারা কোটার নাম")]
         public string LeaseQuotaName { get; set; }
+        [Display(Name = "তৈরিকারী")]
         public string Remarks { get; set; }
+        [Display(Name = "তারিখ তৈরি করুন")]
         public DateTime? CreateDate { get; set; }
+        [Display(Name = "তৈরিকারী")]
         public int? CreatedBy { get; set; }
+        [Display(Name = "সর্বশেষ সংষ্করণ")]
         public DateTime? LastUpdated { get; set; }
+        [Display(Name = "সর্বশেষ সংষ্করণ করেছেন")]
         public int? LastUpdatedBy { get; set; }
+        [Display(Name = "সক্রিয়")]
         public bool? IsActive { get; set; }
+        [Display(Name = "মুছে ফেলা ")]
         public bool? IsDeleted { get; set; }
     }
 }
