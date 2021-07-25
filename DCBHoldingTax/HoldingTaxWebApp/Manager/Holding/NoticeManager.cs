e2fd581e1234858266d51b8eda91dbaf3c06c176
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2021-07-25 15:44:47 +0600

    commit before link to report

---
+++ DCBHoldingTax/HoldingTaxWebApp/Manager/Holding/NoticeManager.cs
@@ -0,0 +1,42 @@
+﻿using HoldingTaxWebApp.Gateway.Holding;
+using HoldingTaxWebApp.Helpers;
+using HoldingTaxWebApp.Models.Holding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Manager.Holding
+{
+    public class NoticeManager
+    {
+        private readonly NoticeGateway _notice;
+        public NoticeManager()
+        {
+            _notice = new NoticeGateway();
+        }
+        public List<Notice> GetAllNotice()
+        {
+            return _notice.GetAllNotice();
+        }
+
+        public List<Notice> GetAllNoticeForHolder(int id)
+        {
+            return _notice.GetAllNoticeForHolder(id);
+        }
+
+        public string SendNotice(Notice model)
+        {
+            int result = _notice.SendNotice(model);
+
+            if (result == 202)
+                return CommonConstantHelper.Success;
+            else if (result == 401)
+                return CommonConstantHelper.Conflict;
+            else if (result == 500)
+                return CommonConstantHelper.Error;
+            else
+                return CommonConstantHelper.Failed;
+        }
+    }
+}
---
+++ DCBHoldingTax/HoldingTaxWebApp/Models/Holding/Notice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Models.Holding
+{
+    public class Notice
+    {
+        public long NoticeId { get; set; }
+        public int FinancialYearId { get; set; }
+        public string FinancialYear { get; set; }
+        public int AreaId { get; set; }
+        public string AreaName { get; set; }
+        public int HolderId { get; set; }
+        public string HolderName { get; set; }
+        public string AreaPlotFlatData { get; set; }
+        public int NoticeTypeId { get; set; }
+        public string NoticeName { get; set; }
+        public string NoticeLinkName { get; set; }
+        public bool? IsNoticeSent { get; set; }
+        public DateTime? NoticeSentDate { get; set; }
+        public string StringNoticeSentDate { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public string CreatedByUsername { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public int? LastUpdatedBy { get; set; }
+        public string UpdatedByUsername { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+
+
+
+        public int? Notice_1 { get; set; }
+        public string NoticeName_1 { get; set; }
+        public int? Notice_2 { get; set; }
+        public string NoticeName_2 { get; set; }
+        public int? Notice_3 { get; set; }
+        public string NoticeName_3 { get; set; }
+    }
+}
