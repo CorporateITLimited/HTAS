3b03c20db2881c83d1fd26dc3e12dc5b74f22931
Rubaid <survivorshanto@gmail.com>
2021-07-27 00:07:46 +0600

    Update clsFinancialYear.cs

--- DCBHoldingTax/HoldingTaxWebApp/Models/DBO/clsFinancialYear.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Models/DBO/clsFinancialYear.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +8,25 @@ namespace HoldingTaxWebApp.Models.DBO
 {
     public class clsFinancialYear
     {
+        [Display(Name = "ফিনান্সিয়াল ইয়ার আইডি")]
         public int FinancialYearId { get; set; }
+        [Display(Name = "ফিনান্সিয়াল ইয়ার")]
         public string FinancialYear { get; set; }
+        [Display(Name = "শুরুর তারিখ")]
         public DateTime? StartingDate { get; set; }
+        [Display(Name = "শেষ তারিখ")]
         public DateTime? EndDate { get; set; }
+        [Display(Name = "তারিখ তৈরি করুন")]
         public DateTime? CreateDate { get; set; }
+        [Display(Name = "তৈরিকারী")]
         public int? CreatedBy { get; set; }
+        [Display(Name = "হালনাগাদকরণ")]
         public DateTime? LastUpdated { get; set; }
+        [Display(Name = "হালনাগাদকরণ করেছেন")]
         public int? LastUpdatedBy { get; set; }
+        [Display(Name = "সক্রিয়")]
         public bool? IsActive { get; set; }
+        [Display(Name = "মুছে ফেলা ")]
         public bool? IsDeleted { get; set; }
     }
 }
