9b8598e6dbf04992dc733a0130468a7f0ed127ec
Mohammad-Hasan92 <56506970+Mohammad-Hasan92@users.noreply.github.com>
2021-07-31 19:30:00 +0600

    Commit ..Create Old Plot Owner
     Create OldPlotOwner Model,
    OtherOldPlotOwner Model,
    View Model,
    OldPlotOwner Gateway
    OldPlotowner Manager
    Done

---
+++ DCBHoldingTax/HoldingTaxWebApp/Models/Plots/OldOthetPlotOwner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Models.Plots
+{
+    public class OldOthetPlotOwner
+    {
+        public int OldOthetPlotOwnerId { get; set; }
+        [Display(Name = "অন্যান্য মালিকের নাম")]
+        public string OldOthetOwneeName { get; set; }
+        public int PlotOwnerId { get; set; }
+        public int OldPlotOwnerId { get; set; }
+        [Display(Name = "প্লট মালিকের নাম")]
+        public string OldPlotOwnerName { get; set; }
+        [Display(Name = "ঠিকানা")]
+        public string Address { get; set; }
+        [Display(Name = "মন্তব্য")]
+
+        public string Remarks { get; set; }
+     
+
+
+    }
+}
---
+++ DCBHoldingTax/HoldingTaxWebApp/Models/Plots/OldPlotOwner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Models.Plots
+{
+    public class OldPlotOwner
+    {
+        public int OldPlotOwnerId { get; set; }
+        public int PlotOwnerId { get; set; }
+
+
+        [Display(Name = "আইডি নম্বর")]
+        public string PlotIdNumber { get; set; }
+        [Display(Name = "প্লট মালিকের নাম")]
+        public string OldPlotOwnerName { get; set; }
+        [Display(Name = "প্লট মালিকের বর্তমান অবস্থা")]
+        public bool? IsAlive { get; set; }
+        [Display(Name = "প্লট মালিকের অফিসিয়াল অবস্থা")]
+        public int OfficialStatusId { get; set; }
+        [Display(Name = "প্লট মালিকের অফিসিয়াল অবস্থা")]
+        public string OffStatusName { get; set; }
+        [Display(Name = "বর্তমান ঠিকানা")]
+        public string PresentAdd { get; set; }
+        [Display(Name = "স্থায়ী ঠিকানা")]
+        public string PermanentAdd { get; set; }
+        [Display(Name = "ফোন")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "ইমেল")]
+        public string Email { get; set; }
+
+        [Display(Name = "প্লটের আয়তন")]
+        public decimal? TotalArea { get; set; }
+
+     
+        [Display(Name = "অন্তর্ভুক্তির তারিখ ")]
+        public DateTime? CreateDate { get; set; }
+        public int? CreatedBy { get; set; }
+        [Display(Name = "অন্তর্ভুক্তকারী")]
+        public string CreatedByUserName { get; set; }
+        [Display(Name = "সর্বশেষ হালনাগাদ ")]
+        public DateTime? LastUpdated { get; set; }
+        [Display(Name = "হালনাগাদকারী  ")]
+        public string UpdatedByUserName { get; set; }
+        public int? LastUpdatedBy { get; set; }
+        [Display(Name = "সক্রিয়তা ")]
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
