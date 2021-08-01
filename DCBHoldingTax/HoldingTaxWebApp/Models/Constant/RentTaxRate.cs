f1bbe8b3d81f99e4bf4c7e1705507996c58ea3bc
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2021-08-01 14:04:00 +0600

    গৃহকরের হার crud
    completed

--- DCBHoldingTax/HoldingTaxWebApp/Models/Constant/RentTaxRate.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Models/Constant/RentTaxRate.cs
@@ -11,29 +11,53 @@ namespace HoldingTaxWebApp.Models.Constant
         public int? RentTaxRateId { get; set; }
         [Display(Name = "এলাকা")]
         public string AreaName { get; set; }
+
+        [Display(Name = "এলাকা")]
         public int? AreaId { get; set; }
+
         [Display(Name = "ভবনের ধরণ ")]
         public string BuildingTypeName { get; set; }
+
+        [Display(Name = "ভবনের ধরণ ")]
         public int? BuildingTypeId { get; set; }
+
         [Display(Name = "প্রতি বর্গফুটের ভাড়া")]
         public decimal PerSqfRent { get; set; }
+
+        [Display(Name = "প্রতি বর্গফুটের ভাড়া")]
+        public string StrPerSqfRent { get; set; }
+
         [Display(Name = "নোট ")]
         public string Remarks { get; set; }
 
 
-        [Display(Name = "তারিখ তৈরি করুন")]
+        [Display(Name = "সংযুক্তির তারিখ ")]
         public DateTime? CreateDate { get; set; }
-        [Display(Name = "তৈরিকারী")]
+
+        [Display(Name = "সংযুক্তির তারিখ ")]
+        public string StrCreateDate { get; set; }
+
+        [Display(Name = "সংযুক্তকারী")]
         public int? CreatedBy { get; set; }
 
-        [Display(Name = "তৈরিকারী")]
-        public string CreatedByName { get; set; }
+        [Display(Name = "সংযুক্তকারী")]
+        public string CreatedByUsername { get; set; }
+
+        [Display(Name = "শেষ হালনাগাদের তারিখ")]
         public DateTime? LastUpdated { get; set; }
+
+        [Display(Name = "শেষ হালনাগাদের তারিখ")]
+        public string StrLastUpdated { get; set; }
+
         [Display(Name = "হালনাগাদকারী")]
-        public string LastUpdatedByName { get; set; }
+        public string UpdatedByUsername { get; set; }
+
+        [Display(Name = "হালনাগাদকারী")]
         public int? LastUpdatedBy { get; set; }
-        [Display(Name = "সক্রিয়")]
+
+        [Display(Name = "সক্রিয়তা")]
         public bool? IsActive { get; set; }
+
         [Display(Name = "মুছে ফেলা ")]
         public bool? IsDeleted { get; set; }
     }
