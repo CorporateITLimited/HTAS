aaa8ff5856184e528928696f349f115f8654de8f
Rubaid <49601746+Rubaid@users.noreply.github.com>
2021-08-31 16:27:59 +0600

    ui of create tax done

--- DCBHoldingTax/HoldingTaxWebApp/Gateway/DBO/FinancialYearGateway.cs
+++ DCBHoldingTax/HoldingTaxWebApp/Gateway/DBO/FinancialYearGateway.cs
@@ -90,6 +90,83 @@ namespace HoldingTaxWebApp.Gateway.Dbo
 
         }
 
+        public List<clsFinancialYear> GetAllFinancialYearForRegenerateTax()
+        {
+            try
+            {
+                Sql_Query = "[dbo].[sptFinancialYearMaster]";
+                Sql_Command = new SqlCommand
+                {
+                    CommandText = Sql_Query,
+                    Connection = Sql_Connection,
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                Sql_Command.Parameters.Clear();
+
+                Sql_Command.Parameters.Add("@StatementType", SqlDbType.NVarChar).Value = "selectnonfinalizetax";
+
+                SqlParameter result = new SqlParameter
+                {
+                    ParameterName = "@result",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Output
+                };
+                Sql_Command.Parameters.Add(result);
+
+
+                Sql_Connection.Open();
+                Data_Reader = Sql_Command.ExecuteReader();
+
+                List<clsFinancialYear> vm = new List<clsFinancialYear>();
+
+                while (Data_Reader.Read())
+                {
+                    clsFinancialYear year = new clsFinancialYear
+                    {
+                        FinancialYearId = Convert.ToInt32(Data_Reader["FinancialYearId"]),
+                        FinancialYear = Data_Reader["FinancialYear"].ToString(),
+                        StartingDate = Data_Reader["StartingDate"] != DBNull.Value ? Convert.ToDateTime(Data_Reader["StartingDate"]) : (DateTime?)null,
+                        EndDate = Data_Reader["EndDate"] != DBNull.Value ? Convert.ToDateTime(Data_Reader["EndDate"]) : (DateTime?)null,
+                        CreateDate = Data_Reader["CreateDate"] != DBNull.Value ? Convert.ToDateTime(Data_Reader["CreateDate"]) : (DateTime?)null,
+                        CreatedBy = Data_Reader["CreatedBy"] !=
+                                                DBNull.Value ? Convert.ToInt32(Data_Reader["CreatedBy"]) : (int?)null,
+                        LastUpdated = Data_Reader["LastUpdated"] != DBNull.Value ? Convert.ToDateTime(Data_Reader["LastUpdated"]) : (DateTime?)null,
+                        LastUpdatedBy = Data_Reader["LastUpdatedBy"] !=
+                                                DBNull.Value ? Convert.ToInt32(Data_Reader["LastUpdatedBy"]) : (int?)null,
+                        IsActive = Data_Reader["IsActive"] != DBNull.Value ? Convert.ToBoolean(Data_Reader["IsActive"]) : (bool?)null,
+                        IsDeleted = Data_Reader["IsDeleted"] != DBNull.Value ? Convert.ToBoolean(Data_Reader["IsDeleted"]) : (bool?)null,
+                    };
+
+                    vm.Add(year);
+                }
+
+                Data_Reader.Close();
+                Sql_Connection.Close();
+
+                return vm;
+            }
+            catch (SqlException exception)
+            {
+                for (int i = 0; i < exception.Errors.Count; i++)
+                {
+                    ErrorMessages.Append("Index #" + i + "\n" +
+                        "Message: " + exception.Errors[i].Message + "\n" +
+                        "Error Number: " + exception.Errors[i].Number + "\n" +
+                        "LineNumber: " + exception.Errors[i].LineNumber + "\n" +
+                        "Source: " + exception.Errors[i].Source + "\n" +
+                        "Procedure: " + exception.Errors[i].Procedure + "\n");
+                }
+                throw new Exception(ErrorMessages.ToString());
+            }
+            finally
+            {
+                if (Sql_Connection.State == ConnectionState.Open)
+                    Sql_Connection.Close();
+            }
+
+        }
+
 
     }
 }
