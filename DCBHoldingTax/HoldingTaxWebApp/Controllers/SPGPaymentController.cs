158ef3f50b0780506fc745993bdbcb5e807c2e01
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2021-11-20 12:18:50 +0600

    commit
    for merge with @almasum77

---
+++ DCBHoldingTax/HoldingTaxWebApp/Controllers/SPGPaymentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoldingTaxWebApp.Controllers
+{
+    public class SPGPaymentController : Controller
+    {
+        private const string _uriStringSessionKey = "https://spg.sblesheba.com:6314/api/SpgService/GetSessionKey";
+        private static readonly HttpClient _apiClient = new HttpClient();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
