609ae96e761e7c5bbfdf73352c8dba18c5f53cce
Rubaid <49601746+Rubaid@users.noreply.github.com>
2021-12-12 16:06:49 +0600

    cluster ui cmmitted

---
+++ DCBHoldingTax/HoldingTaxWebApp/Manager/Users/ClusterManager.cs
@@ -0,0 +1,61 @@
+﻿using HoldingTaxWebApp.Gateway.Users;
+using HoldingTaxWebApp.Helpers;
+using HoldingTaxWebApp.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Manager.Users
+{
+    public class ClusterManager
+    {
+        private readonly ClusterGateway _gateway;
+
+        public ClusterManager()
+        {
+            _gateway = new ClusterGateway();
+        }
+
+        public List<Cluster> GetAllCluster()
+        {
+            return _gateway.GetAllCluster();
+        }
+
+        public Cluster GetClusterById(int id)
+        {
+            return _gateway.GetClusterById(id);
+        }
+
+
+        public string ClusterInsert(Cluster cluster)
+        {
+            int result = _gateway.ClusterInsert(cluster);
+
+            if (result == 202)
+                return CommonConstantHelper.Success;
+            else if (result == 409)
+                return CommonConstantHelper.Conflict;
+            else if (result == 500)
+                return CommonConstantHelper.Error;
+            else
+                return CommonConstantHelper.Failed;
+        }
+
+        public string ClusterUpdate(Cluster cluster)
+        {
+            int result = _gateway.ClusterUpdate(cluster);
+
+            if (result == 202)
+                return CommonConstantHelper.Success;
+            else if (result == 409)
+                return CommonConstantHelper.Conflict;
+            else if (result == 500)
+                return CommonConstantHelper.Error;
+            else
+                return CommonConstantHelper.Failed;
+        }
+
+        
+    }
+}
---
+++ DCBHoldingTax/HoldingTaxWebApp/Models/Users/Cluster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HoldingTaxWebApp.Models.Users
+{
+    public class Cluster
+    {
+        public int ClusterId { get; set; }
+
+        [Display(Name = "ক্লাস্টারের নাম")]
+        public string ClusterName { get; set; }
+
+        [Display(Name = "ক্লাস্টারের বিবরণ")]
+        public string ClusterDetails { get; set; }
+        public int? UserId { get; set; }
+
+        [Display(Name = "ক্লাস্টারের ব্যবস্থাপক")]
+        public string UserFullName { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public int? LastUpdatedBy { get; set; }
+
+        [Display(Name = "সক্রিয়")]
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public string UpdatedByUserName { get; set; }
+        public string CreatedByUserName { get; set; }
+    }
+}
